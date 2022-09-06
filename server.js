652eb29d0c29aa5922df23e64b5f51973e6ef2ac
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-09-06 14:17:57 -0600

    setup basic serverand added index and pricing routes.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static('public'));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+app.get('/pricing', (req, res) => {
+    res.render('pricing.ejs')
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
